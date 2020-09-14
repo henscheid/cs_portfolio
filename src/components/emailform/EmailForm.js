d3c6f12fea929057288f04c632e002a70877d63a
Ian <henscheid@wisc.edu>
2020-09-14 14:14:40 -0500

    contact, email functionality implemented

--- src/components/emailform/EmailForm.js
+++ src/components/emailform/EmailForm.js
@@ -1,18 +1,39 @@
 import React, {Component} from 'react';
+import emailjs from 'emailjs-com';
 import './EmailForm.css';
+
+
 
 class EmailForm extends Component {
   render(){
+    function sendEmail(e) {
+
+
+      
+      e.preventDefault();
+      
+  
+      emailjs.sendForm(
+        'gmail', 
+        'portfolio-email', 
+        e.target,
+        "user_oHKEUiJNqkGdmu9gLwmu5")
+
+        .then((result) => {
+            console.log(result.text);
+        }, (error) => {
+            console.log(error.text);
+        });
+        e.target.reset()
+    }
     return (
       <div id="email-form" className="email-form-container email-form-text">
-        <form id="contact-form">
-          <div id="name-field" className="form-field-container">
-            <input type="text" id="name" name="name" placeholder="Name"></input>
-          </div>
-          <input type="text" id="email" name="email" placeholder="Email"></input><br/>
-          <input type="text" id="message" name="message" placeholder="Message"></input>
+        <form onSubmit={sendEmail}>
+          <input className="input" type="text" id="name" name="name" placeholder="name"/><br/>
+          <input type="email" id="email" name="email" placeholder="email"/><br/>
+          <input type="text" id="message" name="message" placeholder="message"/>
           <br/><br/>
-          <input type="submit" value="Submit"></input>
+          <input type="submit" value="Send Message"></input>
           
         </form>
       </div>
