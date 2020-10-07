00b21e09354de0c0e56c0a355c03fc4ec0e41483
Ian <henscheid@wisc.edu>
2020-10-07 15:29:30 -0500

    made projects collapsible, added global color variables

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -1,4 +1,8 @@
 import React, {Component} from 'react';
+import Collapsible from 'react-collapsible';
+import levelUpLogo from '../../assets/images/leveluplogo.png';
+import viessmanLogo from '../../assets/images/viessmann.png';
+
 import './Projects.css';
 
 class Projects extends Component {
@@ -11,29 +15,32 @@ class Projects extends Component {
         <div className="projects-content">
 
           <div className="ind-project-container">
-            <h2>LevelUp</h2>
-            <ul className="list-temp">
-              <li>experience with React Native, HTML, CSS</li>
-              <li>software lifecycle, agile, sprints</li>
-              <li>working with a team and dividing responsibilites</li>
-            </ul>
+            <Collapsible trigger={<img src={levelUpLogo} alt="Level Up" className="project-logo"/>}>
+              <ul className="list-temp">
+                <li>experience with React Native, HTML, CSS</li>
+                <li>software lifecycle, agile, sprints</li>
+                <li>working with a team and dividing responsibilites</li>
+              </ul>
+            </Collapsible>
           </div>
           <div className="ind-project-container">
-            <h2>Inventory Management (Junior Design Project)</h2>
-            <ul className="list-temp">
-              <li>design thinking and problem oriented thinking</li>
-              <li>working with a team and playing to strengths</li>
-              <li>experience with software and hardware integration</li>
-              <li>using python</li>
-            </ul>
+            <Collapsible trigger={<h2>Inventory Management (Junior Design Project)</h2>}>
+              <ul className="list-temp">
+                <li>design thinking and problem oriented thinking</li>
+                <li>working with a team and playing to strengths</li>
+                <li>experience with software and hardware integration</li>
+                <li>using python</li>
+              </ul>
+            </Collapsible>
           </div>
-          <div className="ind-project-container">
-            <h2>Viessman</h2>
-            <ul className="list-temp">
-              <li>fast paced design thinking environment</li>
-              <li>working with a team, and language barrier</li>
-              <li>presenting ideas to executives, public speaking</li>
-            </ul>
+          <div className="ind-project-container" style={{border:"none"}}>
+            <Collapsible trigger={<img src={viessmanLogo} alt="Level Up" className="project-logo"/>}>
+              <ul className="list-temp">
+                <li>fast paced design thinking environment</li>
+                <li>working with a team, and language barrier</li>
+                <li>presenting ideas to executives, public speaking</li>
+              </ul>
+            </Collapsible>
           </div>
           
 
